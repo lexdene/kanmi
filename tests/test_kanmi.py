46a2511b764c16b0963dce5defa5050e225031a2
Elephant Liu <lexdene@gmail.com>
2019-08-02 22:01:51 +0800

    init project

---
+++ tests/test_kanmi.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from kanmi import Kanmi
+
+
+class TestKanmi(TestCase):
+    def test_simple(self):
+        s1 = Kanmi.create('elephant').to_str()
+
+        k = Kanmi.from_str(s1)
+
+        self.assertTrue(k.validate('elephant'))
+        self.assertFalse(k.validate('rhino'))
